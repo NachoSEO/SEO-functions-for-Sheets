17509a4961915a2d23f0e0e527924aaa8b3b674c
mortola <15958009+mortola@users.noreply.github.com>
2019-05-30 19:38:13 +0200

    Improve code style and fix type coercion potential bug

--- scrapeTags.js
+++ scrapeTags.js
@@ -1,29 +1,26 @@
 // Based on this post: https://codeseo.io/an-old-friend/ from @jroakes
 /**
-* Scrape anything that you want with Regex
-*
-* @customfunction
-*/
+ * Scrape anything that you want with Regex
+ *
+ * @customfunction
+ */
 
 function scrapeTag(url, regex) {
-  if(!url) {
+  if (!url) {
     return "No URL";
-  };
-  
-   var response,code,page,match; 
-  
+  }
+
+  var response, code, page, match;
+
   try {
-  response = UrlFetchApp.fetch(url);
-  page = response.getContentText();
-  code = response.getResponseCode();
-  match = page.match(regex);
-    
-  }catch(e) {
+    response = UrlFetchApp.fetch(url);
+    page = response.getContentText();
+    code = response.getResponseCode();
+    match = page.match(regex);
+
+  } catch (e) {
     return "Error: " + e;
   }
-  
-  if (code == 200) 
-    return match; 
-  else
-    return "URL Issue: Response (" + code + ")";
-};
+
+  return code === 200 ? match : "URL Issue: Response (" + code + ")";
+}
