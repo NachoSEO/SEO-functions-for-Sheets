3a10fb0c3d01b9aa3f9e7ad4ef335928ee0b3411
Nacho Mascort <nacho.mascort@gmail.com>
2020-05-20 17:23:04 +0200

    Create getStatusCode.js

---
+++ getStatusCode.js
@@ -0,0 +1,26 @@
+/**
+ * Get the Status Code of an URL
+ *
+ * @param {string} url The URL to get the status
+ * @return The Status Code of the requested page
+ * @customfunction
+ */
+
+function getStatusCode(url) {
+    const requestHeaders = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36'
+    };
+    
+    const options = {
+      url: url,
+      followRedirects: false,
+      headers: requestHeaders
+    }
+   
+    try {
+      const response = UrlFetchApp.fetch(url, options);
+      return response.getResponseCode();
+    } catch(e) {
+      return 'Error: unable to return any Status Code';
+    }
+  }
