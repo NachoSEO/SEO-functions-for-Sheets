eebb63b21d53696dcc39fbbe71362b6d19be6fb7
Nacho Mascort <nacho.mascort@gmail.com>
2020-05-20 17:19:29 +0200

    Update and rename scrapeTags.js to scrapeByRegex.js

---
+++ scrapeByRegex.js
@@ -0,0 +1,51 @@
+/**
+ * Remove HTML tags
+ *
+ * @param {Array} match The HTML you want to modify
+ * @return The content within HTML removing the HTML tags.
+ * @customfunction
+ */
+
+function removeHtml(match) {
+  const [ firstMatch ] = match;
+  return match.replace(/<[^>]*>?/gmi, "");
+}
+
+/**
+ * Scrape anything that you want with Regex
+ *
+ * @param {string} url The URL you want to scrape
+ * @param {regex} regex Regex that will select a part of the HTML of the URL
+ * @param {boolean} removeTag Set to True if you want to remove the HTML tags
+ * @return The HTML tag with its content or just the inner content.
+ * @customfunction
+ */
+
+function scrapeByRegex(url, regex, removeTag) {
+  if (!url || !regex) {
+    return "Error: Please provide URL & regex";
+  }
+
+  const requestHeaders = {
+    'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+  };
+
+  try {
+    const response = UrlFetchApp.fetch(url, requestHeaders);
+    const statusCode = response.getResponseCode();
+    let match = response
+      .getContentText()
+      .match(regex);
+
+    if (statusCode === 200) {
+      if (removeTag) {
+        match = removeHtml(match);
+      }
+      return match;
+    } else {
+      return `Error: Status code returned: ${statusCode}`;
+    }
+  } catch (e) {
+    return "Error: " + e;
+  }
+}
