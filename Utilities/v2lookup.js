5ae441b6a0f4326016a30105f7f899f9e0a595ba
nachovalen <nachovalen@gmail.com>
2021-04-20 16:02:12 +0200

    Refactor v2lookup

--- Utilities/v2lookup.js
+++ Utilities/v2lookup.js
@@ -1,38 +1,38 @@
 /**
  * Vertical lookup using two elements to find an output
  * 
- * @param {String} firstItem First item to match
- * @param {String} secondItem Second item to match
  * @param {Array} matrix Data to search for
  * @param {Number} index Column where the to find the data
+ * @param {String} firstItem First item to match
+ * @param {String} secondItem Second item to match
  * @returns {String} - The string matched with the specified criteria
  * @customfunction
  */
-function v2lookup(firstItem, secondItem, matrix, index) {
-  let firstItemIndex, lastMatchFirstIndex;
-
+function v2lookup(matrix, index, firstItem, secondItem) {
+  let firstItemFound, secondItemFound;
+  
   let result = "Didn't find a match";
 
-  matrix.forEach((row, i) => {
+  for(const row of matrix) {
     const [ col1, ...cols ] = row;
 
-    if(col1 === firstItem && firstItemIndex === undefined) {
-      firstItemIndex = i;
+    if(col1 === firstItem && firstItemFound === undefined) {
+      firstItemFound = true;
     }
 
-    if(firstItemIndex && !lastMatchFirstIndex) {
+    if(firstItemFound && !secondItemFound) {
       if (col1 !== '' &&  col1 !== firstItem) {
-        lastMatchFirstIndex = i - 1;
+        secondItemFound = true;
       }
     }
 
-    if(firstItemIndex && !lastMatchFirstIndex) {
+    if(firstItemFound && !secondItemFound) {
       if(cols.indexOf(secondItem) !== -1 ) {
-
         result = row[index - 1];
+        break;
       }
     }
-  });
+  }
 
   return result;
 }
